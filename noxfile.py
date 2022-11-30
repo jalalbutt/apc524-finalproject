08ba1f5a538321376c4c085cebd34868f6db2e4b
manuel-k2 <kreutle@princeton.edu>
2022-11-29 21:55:12 -0500

    Project set-up 2

---
+++ noxfile.py
@@ -0,0 +1,11 @@
+import nox
+
+nox.options.sessions = ["lint", "tests"]
+
+@nox.session
+def tests(session: nox.Session) -> None:
+    """
+    Run the unit and regular tests.
+    """
+    session.install(".[test]")
+    session.run("pytest", *session.posargs)
