5acfd5348d5fcbdc15f27bbeaac059e091a62886
manuel-k2 <kreutle@princeton.edu>
2022-12-02 14:41:53 -0500

    Project set-up 12

--- noxfile.py
+++ noxfile.py
@@ -6,7 +6,7 @@ nox.options.sessions = ["tests", "docs", "serve"]
 @nox.session
 def tests(session: nox.Session) -> None:
     """
-    Run the unit and regular tests.
+    Run the unit and regular tests. a
     """
     session.install(".[test]")
     session.run("pytest", *session.posargs)
