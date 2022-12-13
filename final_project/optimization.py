1c7c6576d8be5414204953239fee528c6d516c3c
gmantegna <30732358+gmantegna@users.noreply.github.com>
2022-12-12 22:34:03 -0500

    reformat via pre commit

--- final_project/optimization.py
+++ final_project/optimization.py
@@ -5,7 +5,6 @@ import typing
 
 
 class OptimizedNetwork:
-
     def __init__(
         self,
         A_p: np.ndarray,
@@ -16,37 +15,68 @@ class OptimizedNetwork:
         gas_supply: pd.DataFrame,
         gas_demand: pd.DataFrame,
         pipelines: pd.DataFrame,
-        p_hat_mw:float=100,
-        voltage_angle_cap_radians:float=2*np.pi,
-        slack_node_index:int=0,
-        nse_cost:float=1000,
+        p_hat_mw: float = 100,
+        voltage_angle_cap_radians: float = 2 * np.pi,
+        slack_node_index: int = 0,
+        nse_cost: float = 1000,
     ):
         """
 
-        Initialize OptimizedNetwork with data and validate that inputs are in the correct form.
+        Initialize OptimizedNetwork with data and validate that inputs are
+            in the correct form.
 
         Args:
-            A_p (np.ndarray): Edge-node incidence matrix for power network. Rows are nodes, columns are edges.
-            A_g (np.ndarray): Edge-node incidence matrix for gas network. Rows are nodes, columns are edges.
-            generators (pd.DataFrame): Pandas DF with electric generator characteristics. Columns specified in assert statements.
-            lines (pd.DataFrame): Pandas DF with electric transmission line (edge) characteristics. Columns specified in assert statements.
-            load (pd.DataFrame): Pandas DF with load characteristics. Columns specified in assert statements.
-            gas_supply (pd.DataFrame): Pandas DF with gas supply characteristics. Columns specified in assert statements.
-            gas_demand (pd.DataFrame): Pandas DF with gas demand characteristics. Columns specified in assert statements.
-            pipelines (pd.DataFrame): Pandas DF with gas pipeline characteristics. Columns specified in assert statements.
-            p_hat_mw (float, optional): System base power (MW). Defaults to 100.
-            voltage_angle_cap_radians (float, optional): Maximum voltage angle difference in radians. Defaults to 2*np.pi.
-            slack_node_index (int, optional): Index of slack node (zero-indexed). Defaults to 0.
-            nse_cost (float, optional): Cost of non-served energy. Defaults to 1000.
+            A_p (np.ndarray): Edge-node incidence matrix for power network.
+                Rows are nodes, columns are edges.
+            A_g (np.ndarray): Edge-node incidence matrix for gas network.
+                Rows are nodes, columns are edges.
+            generators (pd.DataFrame): Pandas DF with electric generator
+                characteristics. Columns specified in assert statements.
+            lines (pd.DataFrame): Pandas DF with electric transmission line
+                (edge) characteristics. Columns specified in assert
+                statements.
+            load (pd.DataFrame): Pandas DF with load characteristics. Columns
+                specified in assert statements.
+            gas_supply (pd.DataFrame): Pandas DF with gas supply
+                characteristics. Columns specified in assert statements.
+            gas_demand (pd.DataFrame): Pandas DF with gas demand
+                characteristics. Columns specified in assert statements.
+            pipelines (pd.DataFrame): Pandas DF with gas pipeline
+                characteristics. Columns specified in assert statements.
+            p_hat_mw (float, optional): System base power (MW). Defaults to
+                100.
+            voltage_angle_cap_radians (float, optional): Maximum voltage angle
+                difference in radians. Defaults to 2*np.pi.
+            slack_node_index (int, optional): Index of slack node
+                (zero-indexed). Defaults to 0.
+            nse_cost (float, optional): Cost of non-served energy. Defaults to
+                1000.
         """
         assert list(generators.columns) == [
-            "name","node_p","node_g","is_gas","min_cap_mw","max_cap_mw","fuel_cost_dollars_per_mwh","efficiency_gj_in_per_mwh_out"
+            "name",
+            "node_p",
+            "node_g",
+            "is_gas",
+            "min_cap_mw",
+            "max_cap_mw",
+            "fuel_cost_dollars_per_mwh",
+            "efficiency_gj_in_per_mwh_out",
         ]
-        assert list(lines.columns) == ["from_node","to_node","reactance_pu","capacity_mw"]
-        assert list(load.columns) == ["node","load_mw"]
-        assert list(gas_supply.columns) == ["node_g","supply_gj"]
-        assert list(gas_demand.columns) == ["node_g","demand_gj"]
-        assert list(pipelines.columns) == ["from_node","to_node","capacity_gj","cost"]
+        assert list(lines.columns) == [
+            "from_node",
+            "to_node",
+            "reactance_pu",
+            "capacity_mw",
+        ]
+        assert list(load.columns) == ["node", "load_mw"]
+        assert list(gas_supply.columns) == ["node_g", "supply_gj"]
+        assert list(gas_demand.columns) == ["node_g", "demand_gj"]
+        assert list(pipelines.columns) == [
+            "from_node",
+            "to_node",
+            "capacity_gj",
+            "cost",
+        ]
         assert load.shape[0] == A_p.shape[0]
         assert lines.shape[0] == A_p.shape[1]
         assert gas_supply.shape[0] == A_g.shape[0]
@@ -66,35 +96,35 @@ class OptimizedNetwork:
         self.slack_node_index = slack_node_index
         self.nse_cost = nse_cost
 
-        ## output variables-- recorded after optimization
+        # output variables-- recorded after optimization
 
         # solver status
-        self.out_status:typing.optional(str) = None
+        self.out_status: typing.optional(str) = None
 
         # objective function value
-        self.out_ofv:typing.optional(float) = None
+        self.out_ofv: typing.optional(float) = None
 
         # non served energy for power
-        self.out_nse_power:typing.optional(pd.Series) = None
+        self.out_nse_power: typing.optional(pd.Series) = None
 
         # non served energy for gas
-        self.out_nse_gas:typing.optional(pd.Series) = None
+        self.out_nse_gas: typing.optional(pd.Series) = None
 
         # flow on power lines
-        self.out_flow_p:typing.optional(pd.Series) = None
+        self.out_flow_p: typing.optional(pd.Series) = None
 
         # flow on gas lines
-        self.out_flow_g:typing.optional(pd.Series) = None
+        self.out_flow_g: typing.optional(pd.Series) = None
 
         # generation by generator
-        self.out_generation:typing.optional(pd.Series) = None
+        self.out_generation: typing.optional(pd.Series) = None
 
         # gas supply
-        self.out_gas_supply:typing.optional(pd.Series) = None
+        self.out_gas_supply: typing.optional(pd.Series) = None
 
         # full problem for debugging purposes
         self.out_prob = None
-    
+
     def optimize(self):
         """
         Optimize network and record results
@@ -113,25 +143,25 @@ class OptimizedNetwork:
         v_n = cp.Variable(len(nodes_p))
 
         # generation
-        gen = cp.Variable(len(self.generators),nonneg=True)
+        gen = cp.Variable(len(self.generators), nonneg=True)
 
         # slack generation for power
-        slack_p = cp.Variable(len(nodes_p),nonneg=True)
+        slack_p = cp.Variable(len(nodes_p), nonneg=True)
 
         # gas supply
-        supply_gas = cp.Variable(len(nodes_g),nonneg=True)
+        supply_gas = cp.Variable(len(nodes_g), nonneg=True)
 
         # gas flow
         flow_gas = cp.Variable(len(edges_g))
 
         # gas flow positive component
-        flow_gas_pos = cp.Variable(len(edges_g),nonneg=True)
+        flow_gas_pos = cp.Variable(len(edges_g), nonneg=True)
 
         # gas flow negative component
-        flow_gas_neg = cp.Variable(len(edges_g),nonneg=True)
+        flow_gas_neg = cp.Variable(len(edges_g), nonneg=True)
 
         # slack demand for gas
-        slack_g = cp.Variable(len(nodes_g),nonneg=True)
+        slack_g = cp.Variable(len(nodes_g), nonneg=True)
 
         # expressions
 
@@ -139,7 +169,11 @@ class OptimizedNetwork:
         p_e = []
 
         for e in edges_p:
-            p_e += [self.p_hat_mw * (1/self.lines.reactance_pu[e]) * sum(self.A_p[n,e]*v_n[n] for n in nodes_p)]
+            p_e += [
+                self.p_hat_mw
+                * (1 / self.lines.reactance_pu[e])
+                * sum(self.A_p[n, e] * v_n[n] for n in nodes_p)
+            ]
 
         # constraints
 
@@ -148,8 +182,11 @@ class OptimizedNetwork:
         # power energy balance
         for n in nodes_p:
             constraints += [
-                sum(gen[g] if self.generators.loc[g].node_p == n else 0 for g in gens)
-                - sum(self.A_p[n,e]*p_e[e] for e in edges_p)
+                sum(
+                    gen[g] if self.generators.loc[g].node_p == n else 0
+                    for g in gens
+                )
+                - sum(self.A_p[n, e] * p_e[e] for e in edges_p)
                 - self.load.loc[n].load_mw
                 - slack_p[n]
                 == 0
@@ -157,20 +194,18 @@ class OptimizedNetwork:
 
         # tx line limits
         for e in edges_p:
-            constraints += [
-                -1 * self.lines.loc[e].capacity_mw <= p_e[e]
-            ]
-            constraints += [
-                p_e[e] <= self.lines.loc[e].capacity_mw
-            ]
+            constraints += [-1 * self.lines.loc[e].capacity_mw <= p_e[e]]
+            constraints += [p_e[e] <= self.lines.loc[e].capacity_mw]
 
         # voltage angle differential limits
         for e in edges_p:
             constraints += [
-                -1 * self.voltage_angle_cap_radians <= sum(self.A_p[n,e]*v_n[n] for n in nodes_p)
+                -1 * self.voltage_angle_cap_radians
+                <= sum(self.A_p[n, e] * v_n[n] for n in nodes_p)
             ]
             constraints += [
-                sum(self.A_p[n,e]*v_n[n] for n in nodes_p) <= self.voltage_angle_cap_radians
+                sum(self.A_p[n, e] * v_n[n] for n in nodes_p)
+                <= self.voltage_angle_cap_radians
             ]
 
         # slack bus
@@ -178,21 +213,18 @@ class OptimizedNetwork:
 
         # generator limits
         for g in gens:
-            constraints += [
-                self.generators.loc[g].min_cap_mw <= gen[g]
-            ]
-            constraints += [
-                gen[g] <= self.generators.loc[g].max_cap_mw
-            ]
-            
+            constraints += [self.generators.loc[g].min_cap_mw <= gen[g]]
+            constraints += [gen[g] <= self.generators.loc[g].max_cap_mw]
+
         # gas energy balance
         for n in nodes_g:
             constraints += [
                 supply_gas[n]
-                - sum(self.A_g[n,e]*flow_gas[e] for e in edges_g)
+                - sum(self.A_g[n, e] * flow_gas[e] for e in edges_g)
                 - self.gas_demand.loc[n].demand_gj
                 - sum(
-                    gen[g]*self.generators.loc[g].efficiency_gj_in_per_mwh_out
+                    gen[g]
+                    * self.generators.loc[g].efficiency_gj_in_per_mwh_out
                     if self.generators.loc[g].is_gas
                     else 0
                     for g in gens
@@ -200,48 +232,44 @@ class OptimizedNetwork:
                 - slack_g[n]
                 == 0
             ]
-            
+
         # gas supply limits
         for n in nodes_g:
-            constraints += [
-                0 <= supply_gas[n]
-            ]
-            constraints += [
-                supply_gas[n] <= self.gas_supply.loc[n].supply_gj
-            ]
-            
+            constraints += [0 <= supply_gas[n]]
+            constraints += [supply_gas[n] <= self.gas_supply.loc[n].supply_gj]
+
         # gas flow constraints
         for e in edges_g:
             constraints += [
                 -1 * self.pipelines.loc[e].capacity_gj <= flow_gas[e]
             ]
-            constraints += [
-                flow_gas[e] <= self.pipelines.loc[e].capacity_gj
-            ]
-            
+            constraints += [flow_gas[e] <= self.pipelines.loc[e].capacity_gj]
+
         # set up absolute value variables
         for e in edges_g:
-            constraints += [
-                flow_gas_pos[e] >= flow_gas[e]
-            ]
-            constraints += [
-                flow_gas_neg[e] >= -1 * flow_gas[e]
-            ]
+            constraints += [flow_gas_pos[e] >= flow_gas[e]]
+            constraints += [flow_gas_neg[e] >= -1 * flow_gas[e]]
 
         # objective
         obj = cp.Minimize(
-            sum(gen[g]*self.generators.loc[g].fuel_cost_dollars_per_mwh for g in gens)
-            + sum(slack_p[n]*self.nse_cost for n in nodes_p)
-            + sum((flow_gas_pos[e] + flow_gas_neg[e]) * self.pipelines.loc[e].cost for e in edges_g) 
-            + sum(slack_g[n]*self.nse_cost for n in nodes_g)
+            sum(
+                gen[g] * self.generators.loc[g].fuel_cost_dollars_per_mwh
+                for g in gens
+            )
+            + sum(slack_p[n] * self.nse_cost for n in nodes_p)
+            + sum(
+                (flow_gas_pos[e] + flow_gas_neg[e])
+                * self.pipelines.loc[e].cost
+                for e in edges_g
+            )
+            + sum(slack_g[n] * self.nse_cost for n in nodes_g)
         )
-
 
         # Form and solve problem.
         prob = cp.Problem(obj, constraints)
         prob.solve()
 
-        ## record results
+        # record results
 
         # solver status
         self.out_status = prob.status
@@ -250,22 +278,46 @@ class OptimizedNetwork:
         self.out_ofv = prob.value
 
         # non served energy for power
-        self.out_nse_power = pd.Series(slack_p.value,index=pd.Index(nodes_p,name="node"),name="non-served energy (MWh)")
+        self.out_nse_power = pd.Series(
+            slack_p.value,
+            index=pd.Index(nodes_p, name="node"),
+            name="non-served energy (MWh)",
+        )
 
         # non served energy for gas
-        self.out_nse_gas = pd.Series(slack_g.value,index=pd.Index(nodes_g,name="node"),name="non-served energy (GJ)")
+        self.out_nse_gas = pd.Series(
+            slack_g.value,
+            index=pd.Index(nodes_g, name="node"),
+            name="non-served energy (GJ)",
+        )
 
         # flow on power lines
-        self.out_flow_p = pd.Series([x.value for x in p_e],index=pd.Index(nodes_p,name="node"),name="Power flow by line (MW)")
+        self.out_flow_p = pd.Series(
+            [x.value for x in p_e],
+            index=pd.Index(nodes_p, name="node"),
+            name="Power flow by line (MW)",
+        )
 
         # flow on gas lines
-        self.out_flow_g = pd.Series(flow_gas.value,index=pd.Index(nodes_g,name="node"),name="Gas flow by line (GJ)")
+        self.out_flow_g = pd.Series(
+            flow_gas.value,
+            index=pd.Index(nodes_g, name="node"),
+            name="Gas flow by line (GJ)",
+        )
 
         # generation by generator
-        self.out_generation = pd.Series(gen.value,index=pd.Index(gens,name="generator"),name="Generation by generator (MWh)")
+        self.out_generation = pd.Series(
+            gen.value,
+            index=pd.Index(gens, name="generator"),
+            name="Generation by generator (MWh)",
+        )
 
         # gas supply
-        self.out_gas_supply = pd.Series(supply_gas.value,index=pd.Index(nodes_g,name="node"),name="Gas supply (GJ)")
+        self.out_gas_supply = pd.Series(
+            supply_gas.value,
+            index=pd.Index(nodes_g, name="node"),
+            name="Gas supply (GJ)",
+        )
 
         # full problem for debugging purposes
         self.out_prob = prob
--- tests/test_optimization.py
+++ tests/test_optimization.py
@@ -2,48 +2,51 @@ import optimization
 import numpy as np
 import pandas as pd
 
+
 def test_optimization():
     """
     Test optimization with known test case (made up data for small network)
     """
 
-    ### set up parameters
-    A_p = np.array([[1,0,-1],[-1,1,0],[0,-1,1]]) #edge-node incidence matrix for power
-    A_g = A_p #same for gas
+    # set up parameters
+    A_p = np.array(
+        [[1, 0, -1], [-1, 1, 0], [0, -1, 1]]
+    )  # edge-node incidence matrix for power
+    A_g = A_p  # same for gas
 
     generators = pd.DataFrame()
-    generators["name"] = ["coal","gas","pv"]
-    generators["node_p"] = [0,1,2]
-    generators["node_g"] = [0,1,2]
-    generators["is_gas"] = [False,True,False]
-    generators["min_cap_mw"] = [0,0,0]
-    generators["max_cap_mw"] = [100,200,50]
-    generators["fuel_cost_dollars_per_mwh"] = [5,10,50]
-    generators["efficiency_gj_in_per_mwh_out"] = [5,5,5]
+    generators["name"] = ["coal", "gas", "pv"]
+    generators["node_p"] = [0, 1, 2]
+    generators["node_g"] = [0, 1, 2]
+    generators["is_gas"] = [False, True, False]
+    generators["min_cap_mw"] = [0, 0, 0]
+    generators["max_cap_mw"] = [100, 200, 50]
+    generators["fuel_cost_dollars_per_mwh"] = [5, 10, 50]
+    generators["efficiency_gj_in_per_mwh_out"] = [5, 5, 5]
 
     lines = pd.DataFrame()
-    lines["from_node"] = [0,1,2]
-    lines["to_node"] = [1,2,0]
-    lines["reactance_pu"] = [0.3,0.3,0.3]
-    lines["capacity_mw"] = [500,500,500]
+    lines["from_node"] = [0, 1, 2]
+    lines["to_node"] = [1, 2, 0]
+    lines["reactance_pu"] = [0.3, 0.3, 0.3]
+    lines["capacity_mw"] = [500, 500, 500]
 
     load = pd.DataFrame()
-    load["node"] = [0,1,2]
-    load["load_mw"] = [0,0,40]
+    load["node"] = [0, 1, 2]
+    load["load_mw"] = [0, 0, 40]
 
     gas_supply = pd.DataFrame()
-    gas_supply["node_g"] = [0,1,2]
-    gas_supply["supply_gj"] = [1000,1000,1000] #exajoules
+    gas_supply["node_g"] = [0, 1, 2]
+    gas_supply["supply_gj"] = [1000, 1000, 1000]  # exajoules
 
     gas_demand = pd.DataFrame()
-    gas_demand["node_g"] = [0,1,2]
-    gas_demand["demand_gj"] = [100,100,100]
+    gas_demand["node_g"] = [0, 1, 2]
+    gas_demand["demand_gj"] = [100, 100, 100]
 
     pipelines = pd.DataFrame()
-    pipelines["from_node"] = [0,1,2]
-    pipelines["to_node"] = [1,2,0]
-    pipelines["capacity_gj"] = [100,100,100]
-    pipelines["cost"] = [5,5,5]
+    pipelines["from_node"] = [0, 1, 2]
+    pipelines["to_node"] = [1, 2, 0]
+    pipelines["capacity_gj"] = [100, 100, 100]
+    pipelines["cost"] = [5, 5, 5]
 
     network = optimization.OptimizedNetwork(
         A_p=A_p,
