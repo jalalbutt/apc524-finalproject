5b3c5a1a5eb1d9421cfa9697e972939a29c462e0
manuel-k2 <kreutle@princeton.edu>
2022-12-15 17:47:40 -0500

    Dash set-up 2.2

--- final_project/dash/app.py
+++ final_project/dash/app.py
@@ -1,11 +1,29 @@
 from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
+import random
+import networkx as nx
 
-df = pd.read_csv(
+timesteps = pd.Series(range(5))
+
+df_density = pd.read_csv(
     "https://raw.githubusercontent.com/plotly/"
-    "datasets/master/gapminderDataFiveYear.csv"
+    "datasets/master/earthquakes-23k.csv"
 )
+us_cities = pd.read_csv(
+    "https://raw.githubusercontent.com/plotly/"
+    "datasets/master/us-cities-top-1k.csv"
+)
+
+g = nx.Graph()
+g.add_nodes_from(us_cities["City"].tolist())
+
+i = 0
+while i < 20:
+    randomCities = random.sample(us_cities["City"].tolist(), 2)
+    g.add_edge(randomCities[0], randomCities[1], capacity=random.random())
+    i += 1
 
 app = Dash(__name__)
 
@@ -15,11 +33,11 @@ app.layout = html.Div(
     [
         dcc.Graph(id=gws),
         dcc.Slider(
-            df["year"].min(),
-            df["year"].max(),
+            timesteps.min(),
+            timesteps.max(),
             step=None,
-            value=df["year"].min(),
-            marks={str(year): str(year) for year in df["year"].unique()},
+            value=timesteps.min(),
+            marks={str(year): str(year) for year in timesteps.unique()},
             id="year-slider",
         ),
     ]
@@ -30,15 +48,6 @@ app.layout = html.Div(
 def update_figure(selected_year):
     # filtered_df = df[df.year == selected_year]
 
-    df_density = pd.read_csv(
-        "https://raw.githubusercontent.com/plotly/"
-        "datasets/master/earthquakes-23k.csv"
-    )
-    us_cities = pd.read_csv(
-        "https://raw.githubusercontent.com/plotly/"
-        "datasets/master/us-cities-top-1k.csv"
-    )
-
     fig = px.density_mapbox(
         df_density,
         lat="Latitude",
@@ -47,6 +56,7 @@ def update_figure(selected_year):
         radius=10,
         center=dict(lat=0, lon=180),
         zoom=0,
+        color_continuous_scale=px.colors.diverging.RdGy,
         mapbox_style="stamen-terrain",
     )
 
@@ -56,10 +66,13 @@ def update_figure(selected_year):
         lon="lon",
         hover_name="City",
         hover_data=["State", "Population"],
-        color="lat",  # color_continuous_scale="Viridis", range_color=(0, 20),
-        zoom=3,
+        color="lat",
+        color_continuous_scale="Viridis",  # range_color=(0, 20),
+        # size = "Population", size_max=400000,
+        zoom=100,
         height=400,
     )
+    fig2.update_traces(marker={"size": 135})
 
     fig.add_trace(fig2.data[0])
     fig.layout.coloraxis2 = fig2.layout.coloraxis
@@ -71,9 +84,36 @@ def update_figure(selected_year):
         "sizemode": "area",
         "sizeref": 0.01,
         "autocolorscale": False,
+        "size": 10,
     }
 
     fig.layout.coloraxis2.colorbar.x = -0.05
+    fig.layout.coloraxis.colorbar.x = -0.1
+
+    for edge in g.edges():
+        city1 = us_cities[us_cities["City"] == edge[0]]
+        city2 = us_cities[us_cities["City"] == edge[1]]
+
+        edgecities = pd.concat((city1.iloc[[0]], city2.iloc[[0]]))
+
+        capacity = g[edge[0]][edge[1]]["capacity"]
+        if capacity < 0.3:
+            color = "#FF0000"
+        if 0.1 <= capacity < 0.6:
+            color = "#FF7F00"
+        if 0.6 <= capacity:
+            color = "#00FF00"
+
+        fig.add_trace(
+            go.Scattermapbox(
+                lat=edgecities["lat"],
+                lon=edgecities["lon"],
+                mode="lines",
+                line={"color": color},
+                text="Capacity =" + str(capacity),
+                hoverinfo="text",
+            )
+        )
 
     fig.update_layout(
         mapbox_style="carto-positron",
