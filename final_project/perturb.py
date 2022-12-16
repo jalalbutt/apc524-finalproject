c46d1fad9d9f5137444126fc98bd4d3adfff8b0f
jalalbutt <jbutt@pppl.gov>
2022-12-15 22:03:14 -0500

    1.) Fixed source_center connection in source construction; 2.) removed __init__() for @dataclass class; 3.) popualated __post_init__() w input tests; 4.) removed file's hardcoded inputs; 5.) added example usage.

--- final_project/perturb.py
+++ final_project/perturb.py
@@ -11,11 +11,6 @@ Module file handling the perturbation network
 ----------------------------------------------
 
 
-Inputs:
--------
-	- grid obj 
-
-
 Outputs:
 --------
 	- time-evolution of perturbation
@@ -33,30 +28,25 @@ import scipy.sparse.linalg as linalg
 # from scipy import signal
 
 from dataclasses import dataclass
+import traceback
 
 
+## Arb parameters for testing.
 
 ## external inputs, to be fed in from 
+"""
 m = 100 # number of grid-points
-# n = 100 #  currently not implemented
+n = 100
+n = 100 #  currently not implemented
 L_x = 10
 H_y = 10
 
-# f_type = 'oscillatory'
+f_type = 'oscillatory'
 f_type = 'delta'
 source_point = (-2,-2)
 source_strength = 1000
-
-plotSolution = True
-plotError = False
-
-
-
-
-# Problem specification
-x = np.linspace(0, L_x, m + 2)
-y = np.linspace(0, H_y, m + 2)
-delta_x = L_x / (m + 1)
+radius = 10
+"""
 
 
 @dataclass
@@ -66,8 +56,15 @@ class PerturbedNetwork:
 
 	Inputs:
 	------
-		m
-		...
+		f_type :   forcing function type (from a finite set)
+		m      :   # of grid points
+		radius :   perturbation extent in terms of grid [points] 
+		source_center :   coordinate pair; % of (L_x, H_y) (not indices)
+
+
+	Outputs:
+	-------
+		U :   np.array (2D if len(timesteps)== 1); 3D if > 1)
 
 	Methods:
 	--------
@@ -81,28 +78,58 @@ class PerturbedNetwork:
 		static_solve:   compute the time-independent solution
 
 
+	Usage:
+	-----
+	Example:
+		m = 100 # number of grid-points
+		n = 100
+		n = 100 #  currently not implemented
+		L_x = 10
+		H_y = 10
+
+		f_type = 'oscillatory'
+		f_type = 'delta'
+		source_center=  = ()
+		source_strength = 1000
+		radius = m/L_x* 0.1
+
+		pertNet2 = PerturbedNetwork(f_type= f_type, source_point= source_center, 
+		                            m= m, L_x= L_x, H_y= H_y, radius= radius)
+		U = pertNet2.static_solve()
+		pertNet2.plot_solution()
 
 
 	"""
-	def __init__(self, f_type, source_point, L_x, H_y, m, source_strength= 1000):
-		self.f_type          = f_type
-		self.source_point    = source_point
-		self.source_strength = source_strength
-		self.m   = m
-		self.L_x = L_x
-		self.H_y = H_y
+
+	## Inputs:
+	f_type: str
+	source_center: [tuple,list,np.ndarray]  
+	m: int
+	L_x: int
+	H_y: int
+	radius: float = m/L_x* 0.01
+	source_strength: float = 1000
+
+	def __post_init__(self):  # what should i do w this? generate tests?
+		self.failure = False
+		self.delta_x = L_x / (m + 1)
+		self.x       = np.linspace(0, self.L_x, self.m + 2)
+		self.y       = np.linspace(0, self.H_y, self.m + 2)
+
+		try:
+			assert (len(source_point) == 2), "Source point must be coordinate pair"
+			assert (m > 10), "Grid must be AT LEAST 10x10"
+			assert (radius < 0.1*m), "Disturbance radius cannot span >10% of grid"
+			assert (f_type in ['delta', 'oscillatory']), "Requested source-type is not yet configured."
+			assert (radius > 0.6*delta_x), "Source cannot be resolved on the grid due to small radius"
+			
+		except:
+			traceback.print_exc()
+			self.failure= True
+			self.intialization_failure_message = "Cannot complete this action due to initialization failure"
 
 
-		self.x = np.linspace(0, L_x, m + 2)
-		self.y = np.linspace(0, H_y, m + 2)
-		self.delta_x = L_x / (m + 1)		
 
-
-
-
-
-	def __post_init__():  # what should i do w this? generate tests?
-		pass
 
 
 	# Construct A
@@ -135,7 +162,9 @@ class PerturbedNetwork:
 
 
 	# Right-hand-side (forcing-function)
-	def construct_source(self, x,y, f_type= 'oscillatory', source_point = (-2,-2),source_strength= 1, impulse_extent= 30, center= (10,12), radius= 2):
+	def construct_source(self, x,y, f_type= 'oscillatory',
+	                     source_strength= 1, impulse_extent= 30, source_center= (10,12), 
+	                     radius= 2):
 		"""
 		Construct source (i.e. bomb source)
 		
@@ -146,6 +175,7 @@ class PerturbedNetwork:
 			- oscillatory could correspond to a heat-wave.
 		source_point     : 
 		source_strength  : 
+		radius : 
 
 		Outputs:
 		-------
@@ -163,7 +193,8 @@ class PerturbedNetwork:
 			# for i in range(impulse_extent):
 			# 	f += signal.unit_impulse(X_source.shape, sp)*source_strength
 			# 	sp = (sp[0]-1, sp[1]-1)
-			circle = np.sqrt((X_source-center[1])**2 + (Y_source-center[0])**2)
+			circle = np.sqrt((X_source-source_center[1]*self.L_x)**2 \
+			                 + (Y_source-source_center[0]*self.H_y)**2)
 			f      = np.zeros(X_source.shape)
 			f[circle<radius] += source_strength
 
@@ -233,6 +264,8 @@ class PerturbedNetwork:
 
 
 	def plot_solution(self):#, X,Y,U, L_x, H_y):
+		if self.failure: return print(self.intialization_failure_message)
+
 		# Plot solution
 		fig1 = plt.figure()
 		axes1 = fig1.add_subplot(1, 1, 1)
@@ -280,19 +313,20 @@ class PerturbedNetwork:
 		U
 
 		"""
+		if self.failure: return print(self.intialization_failure_message)
 
 		self.A = self.construct_A(self.m, self.delta_x)
 
 		# what is this X,Y used for besides source construction?
 		self.X_source,self.Y_source,\
 		self.f = self.construct_source(self.x,self.y,f_type = self.f_type, 
-		                                      source_point= self.source_point, 
 		                                      source_strength= self.source_strength,
-		                                      center= (H_y-1,L_x-1), radius= 1)
-		self.U = self.solve_laplacian_system(m,self.A,self.f)
+		                                      source_center= self.source_center, 
+		                                      radius= self.radius)
+		self.U = self.solve_laplacian_system(self.m,self.A,self.f)
 
 		self.X,self.Y,self.norm_error,\
-		self.grid_error = self.compute_discretization_error(x,y,self.U)
+		self.grid_error = self.compute_discretization_error(self.x,self.y,self.U)
 
 		return self.U
 
